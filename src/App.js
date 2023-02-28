8d9c86a5374f554e78f993ff4761c13482ba4df6
youngsikwon93@naver.com <youngsik3200@gmail.com>
2023-03-01 00:55:08 +0900

    리액트 기초 작업

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
+//  useState를 활용하여 3개의 블로그 목록을 만들기 Part 1
 function App() {
+  let post = "강남 고기 맛집";
+
+  let [title, setTitle] = useState(" dev blog ");
+
+  let [a, b] = useState([" 강  남 맛집 ", "신도림 맛집", "영등포 맛집"]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>{title}</h4>
+      </div>
+
+      <div className="list">
+        <h4>{a[0]}</h4>
+        <p>-</p>
+      </div>
+
+      <div className="list">
+        <h4>{a[1]}</h4>
+        <p>-</p>
+      </div>
+
+      <div className="list">
+        <h4>{[2]}</h4>
+        <p>-</p>
+      </div>
     </div>
   );
 }
